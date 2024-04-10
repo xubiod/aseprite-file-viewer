174553f1da01d9c42e7799fcfdbf706283dce123
Xubiod <xubiod.mail@gmail.com>
2024-04-10 13:19:48 -0400

    i forgot to use offset

--- pride_effect.gd
+++ pride_effect.gd
@@ -12,8 +12,8 @@ func _process_custom_fx(char_fx: CharFXTransform) -> bool:
 	var stretch : float = 1. / char_fx.env.get("stretch", 5.)
 	var offset : float = char_fx.env.get("offset", 0.)
 
-	var progress0 : float = wrapf(((float(char_fx.relative_index) * stretch) / float(char_fx.glyph_count)) + (char_fx.elapsed_time / spd), 0, float(len(colour)))
-	var progress1 : float = wrapf(((float(char_fx.relative_index + 1) * stretch) / float(char_fx.glyph_count)) + (char_fx.elapsed_time / spd), 0, float(len(colour)))
+	var progress0 : float = wrapf(((float(char_fx.relative_index) * stretch + offset) / float(char_fx.glyph_count)) + (char_fx.elapsed_time / spd), 0, float(len(colour)))
+	var progress1 : float = wrapf(((float(char_fx.relative_index + 1) * stretch + offset) / float(char_fx.glyph_count)) + (char_fx.elapsed_time / spd), 0, float(len(colour)))
 
 	char_fx.color = colour[int(progress0)]#.lerp(colour[int(progress1)], abs(progress0 - progress1))
 
