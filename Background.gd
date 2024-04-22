019147631b173113ac966c79cb38f295a573abdf
Xubiod <xubiod.mail@gmail.com>
2024-04-22 14:07:18 -0400

    background is complicated, hide it on lost focus

--- Background.gd
+++ Background.gd
@@ -7,3 +7,10 @@ func _ready() -> void:
 	pixel_size = init_size
 
 func _on_left_side_ui_zoom_changed(value: float) -> void: pixel_size = init_size / value
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_APPLICATION_FOCUS_IN:
+			visible = true
+		NOTIFICATION_APPLICATION_FOCUS_OUT:
+			visible = false
