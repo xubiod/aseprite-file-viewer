a4db21bfca56e127de124131159456d13b1e3fd7
Xubiod <xubiod.mail@gmail.com>
2024-04-22 13:54:33 -0400

    more notifications; drop fps while unfocused

---
+++ GlobalNotificationHandler.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_APPLICATION_FOCUS_IN:
+			# print("got focus")
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+			Engine.max_fps = 0
+		NOTIFICATION_APPLICATION_FOCUS_OUT:
+			# print("lost focus")
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+			Engine.max_fps = 5
